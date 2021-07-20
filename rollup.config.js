4b3f6d2e220b4a057afe152a27c8d520b9fa50e6
Luke Edwards <luke.edwards05@gmail.com>
2021-07-19 21:37:53 -0700

    fix: attach `commonjs` plugin for libpcre

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
 
 /**
  * @type {import('rollup').RollupOptions}
@@ -13,7 +14,12 @@ const config = {
     sourcemap: false,
     interop: false,
   },
-  plugins: [resolve({ browser: true }), typescript()],
+  // prettier-ignore
+  plugins: [
+    resolve({ browser: true }),
+    commonjs(),
+    typescript(),
+  ],
 }
 
 export default config
